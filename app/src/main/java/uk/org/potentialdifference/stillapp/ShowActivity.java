68a965963ebaade233356d362b671e8461ff37ee
Henry Garner <henry@henrygarner.com>
2015-11-07 20:32:54 +0000

    Talking over ws not wss

--- app/src/main/java/uk/org/potentialdifference/stillapp/ShowActivity.java
+++ app/src/main/java/uk/org/potentialdifference/stillapp/ShowActivity.java
@@ -1,5 +1,7 @@
 package uk.org.potentialdifference.stillapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,8 +11,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
 import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_10;
 import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -19,6 +26,7 @@ public class ShowActivity extends AppCompatActivity {
 
     private static String TAG = "ShowActivity";
     WebSocketClient mWebSocketClient;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +44,15 @@ public class ShowActivity extends AppCompatActivity {
             }
         });
 
+        imageView = (ImageView) findViewById(R.id.imageView);
+
         setupWebSockets();
     }
 
     protected void setupWebSockets() {
         URI uri;
         try {
-            uri = new URI("ws://192.168.0.6:9292");
+            uri = new URI("wss://192.168.0.6:8081");
         } catch (URISyntaxException e) {
             Log.d(TAG, "error creating URI");
             e.printStackTrace();
@@ -50,16 +60,41 @@ public class ShowActivity extends AppCompatActivity {
         }
 
         Log.d(TAG, "creating websocket client...");
-        mWebSocketClient = new WebSocketClient(uri) {
+        mWebSocketClient = new WebSocketClient(uri, new Draft_10()) {
             @Override
             public void onOpen(ServerHandshake serverHandshake) {
-                Log.i("Websocket", "Opened");
-                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+                Log.i(TAG, "Websocket onOpen called");
             }
 
             @Override
             public void onMessage(String s) {
-                Log.d(TAG, "message received: " + s);
+                Log.d(TAG, "websocket message " + s);
+                try {
+                    Log.d(TAG, "message received: " + s);
+                    JSONObject jsonObj = new JSONObject(s);
+                    String message = jsonObj.getString("message");
+                    Log.d(TAG, "got message type: " + message);
+                    if (message.equals("displayImage")) {
+                        Log.d(TAG, "setting new display image");
+                        String path = jsonObj.getString("path");
+                        final String uri = "http://192.168.0.6:8081/" + path;
+                        final Uri imageUri = Uri.parse(uri);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Bitmap bmp = BitmapFactory.decodeStream(new java.net.URL(uri).openStream());
+                                    imageView.setImageBitmap(bmp);
+                                } catch (Exception e) {
+                                    Log.e(TAG, e.getMessage());
+                                }
+                            }
+                        });
+                        Log.d(TAG, "setting new display image to " + uri);
+                    }
+                } catch (JSONException e) {
+                    Log.e(TAG, "json exception: " + e.toString());
+                }
             }
 
             @Override
@@ -74,6 +109,7 @@ public class ShowActivity extends AppCompatActivity {
         };
         Log.d(TAG, "created websocket client");
         mWebSocketClient.connect();
+
     }
 
 }
